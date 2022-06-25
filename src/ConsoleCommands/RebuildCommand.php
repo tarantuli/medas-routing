bb20bad436b38e8c411b535e177e69b59acef430
Patrick Morpey <patrick@morphp.nl>
2022-06-25 16:30:19 +0200

    Renamed file, added Service attribute

--- src/ConsoleCommands/RebuildCommand.php
+++ src/ConsoleCommands/RebuildCommand.php
@@ -4,12 +4,13 @@ declare(strict_types=1);
 
 namespace Medas\Routing\ConsoleCommands;
 
-use Medas\Console\Commands\BaseConsoleCommand;
-use Medas\Console\Commands\ConsoleCommandGroup;
+use Medas\Console\Commands\{BaseConsoleCommand, ConsoleCommandGroup};
 use Medas\Console\Printer;
 use Medas\Routing\HandlerManager;
+use Medas\ServiceManager\Attributes\Service;
 
-class ReloadCommand extends BaseConsoleCommand
+#[Service]
+class RebuildCommand extends BaseConsoleCommand
 {
     public function __construct(
         private readonly HandlerManager $handlerManager,
